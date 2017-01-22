df7ac4013513a934360739e8500b9adb92a1861e
apprentice man <apprenticexn@gmail.com>
2017-01-22 13:47:03 +0900

    Update BonusDrink::self.total_count_for

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,10 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    result = balance_for_bonus = amount
+    while 0 <= balance_for_bonus -= 3
+      result += 1
+      balance_for_bonus += 1
+    end
+    result
   end
 end
